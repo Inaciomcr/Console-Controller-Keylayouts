dbfe12ea3f7c200fde2fd991170caf07e916e939
Ben Katkin <benjaminkatkin@gmail.com>
2016-10-04 11:51:34 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -36,7 +36,7 @@ CACHEMOD=false
 
 # This will be the folder name under /magisk or /cache/magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=template
+MODID=xboxkeylayouts
 
 # Set to true if you need automount
 # Most mods would like it to be enabled
@@ -58,9 +58,10 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
-  ui_print "*******************************"
+  ui_print "********************************"
+  ui_print "      Xbox Keylayouts v1.0      "
+  ui_print "   by HylianGaming AKA Nbay64   "
+  ui_print "********************************"
 }
 
 ##########################################################################################
