566dde13413ebf2fea63feea6c995ae36c9cd996
whosucks <wingzeroh@live.ca>
2017-07-15 01:19:25 -0400

    Add files via upload

--- config.sh
+++ config.sh
@@ -1,27 +1,19 @@
 ##########################################################################################
 #
-# Magisk
+# Magisk Module Template Config Script
 # by topjohnwu
 # 
-# This is a template zip for developers
-#
 ##########################################################################################
 ##########################################################################################
 # 
 # Instructions:
 # 
-# 1. Place your files into system folder
-# 2. Fill in all sections in this file
-# 3. For advanced features, add commands into the script files under common:
-#    post-fs.sh, post-fs-data.sh, service.sh
-# 4. Change the "module.prop" under common with the info of your module
-# 
-##########################################################################################
-##########################################################################################
-# 
-# Limitations:
-# 1. Can not place any new items under /system root!! e.g. /system/newfile, /system/newdir
-#    Magisk will delete these items at boot.
+# 1. Place your files into system folder (delete the placeholder file)
+# 2. Fill in your module's info into module.prop
+# 3. Configure the settings in this file (common/config.sh)
+# 4. For advanced features, add shell commands into the script files under common:
+#    post-fs-data.sh, service.sh
+# 5. For changing props, add your additional/modified props into common/system.prop
 # 
 ##########################################################################################
 
@@ -31,26 +23,22 @@
 
 # NOTE: This part has to be adjusted to fit your own needs
 
-# Is this a cache mod?
-CACHEMOD=false
-
-# This will be the folder name under /magisk or /cache/magisk
+# This will be the folder name under /magisk
 # This should also be the same as the id in your module.prop to prevent confusion
 MODID=consolekeylayouts
 
-# Set to true if you need automount
+# Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
 AUTOMOUNT=true
 
-# Set to true if you need post-fs script (Only available in cache mods)
-POSTFS=false
+# Set to true if you need to load system.prop
+PROPFILE=false
 
-# Set to true if you need post-fs-data script (Only available in non-cache mods)
+# Set to true if you need post-fs-data script
 POSTFSDATA=false
 
-# Set to true if you need late_start service script (Only available in non-cache mods)
+# Set to true if you need late_start service script
 LATESTARTSERVICE=false
-
 ##########################################################################################
 # Installation Message
 ##########################################################################################
@@ -58,10 +46,9 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "**********************************************"
-  ui_print "      Console Controller Keylayouts v1.0      "
-  ui_print "          by HylianGaming AKA Nbay64          "
-  ui_print "**********************************************"
+  ui_print "*******************************"
+  ui_print "     Magisk Module Template    "
+  ui_print "*******************************"
 }
 
 ##########################################################################################
@@ -72,6 +59,7 @@ print_modname() {
 # By default Magisk will merge your files with the original system
 # Directories listed here however, will be directly mounted to the correspond directory in the system
 
+# You don't need to remove the example below, these values will be overwritten by your own list
 # This is an example
 REPLACE="
 /system/app/Youtube
@@ -80,12 +68,13 @@ REPLACE="
 /system/framework
 "
 
-# Construct your own list
+# Construct your own list here, it will overwrite the example
+# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
 "
 
 ##########################################################################################
-# Permissons
+# Permissions
 ##########################################################################################
 
 # NOTE: This part has to be adjusted to fit your own needs
@@ -94,7 +83,7 @@ set_permissions() {
   # Default permissions, don't remove them
   set_perm_recursive  $MODPATH  0  0  0755  0644
 
-  # Only some special files require specific permission settings
+  # Only some special files require specific permissions
   # The default permissions should be good enough for most cases
 
   # Some templates if you have no idea what to do:
@@ -106,7 +95,3 @@ set_permissions() {
   # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
   # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
   # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
-  set_perm  $MODPATH/system/usr/keylayout/Vendor_045e_Product_02d1.kl       0       0       0644
-  set_perm  $MODPATH/system/usr/keylayout/Vendor_045e_Product_02e0.kl       0       0       0644
-  set_perm  $MODPATH/system/usr/keylayout/Vendor_045e_Product_0291.kl       0       0       0644
-}
